54a6f6714862ec36b6587db8b3c8308dc606a002
parseyoung <104110839+parseyoung@users.noreply.github.com>
2024-02-04 21:28:04 +0900

    2024.02.04

--- AI_Volume_Control/main.py
+++ AI_Volume_Control/main.py
@@ -1,7 +1,7 @@
 import cv2
 import time
 import numpy as np
-import hand_detector as htm
+import HandTrackingModule as htm
 import math
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
